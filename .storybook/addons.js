dcf0fd1bca0b84ce4c7bc28f0b25096209e2fdca
idimetrix <selikhov.dmitrey@gmail.com>
2024-09-17 00:53:24 -0400

    -

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,4 @@
-import '@storybook/addon-knobs/register';
+import '@storybook/addon-knobs';
 import '@storybook/addon-options/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-notes/register';
